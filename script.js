c4edc0c53921ff26eba49f73db3941abba44dd9b
lassev05 <vestergaardlasse2@gmail.com>
2022-01-06 20:52:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const navbar = document.querySelector("#navbar");
+const navTitle = navbar.querySelector("#nav-title");
+const navList = navbar.querySelector("#nav-list");
+const navA = navbar.querySelector("#nav-a");
+const body = document.querySelector("#body");
+
+body.classList.add("bg-black");
+
+navbar.classList.add("navbar-white");
+navTitle.classList.add("site-title");
+navList.classList.add("display-flex");
+navA.classList.add("ml-1");
